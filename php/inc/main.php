08eee81310634cf38d405ca7d380a0ded9b81c07
SEO <malacma@gmail.com>
2017-04-06 02:56:50 -0300

    Mautic Tracking Code

--- php/inc/main.php
+++ php/inc/main.php
@@ -129,6 +129,24 @@
                         $("#tabDocs").show();
                         $("#tabAvisos").show();
                     }
+
+                    (function(w, d, t, u, n, a, m) {
+                        w['MauticTrackingObject'] = n;
+                        w[n] = w[n] || function() {
+                            (w[n].q = w[n].q || []).push(arguments)
+                        }, a = d.createElement(t),
+                                m = d.getElementsByTagName(t)[0];
+                        a.async = 1;
+                        a.src = u;
+                        m.parentNode.insertBefore(a, m)
+                    })(window, document, 'script', 'https://inbound.citywatch.com.br/mtc.js', 'mt');
+                    mt(
+                            'send',
+                            'pageview',
+                            {email: '<?php echo $rperfil[2]; ?>', firstname: '<?php echo $rperfil[1]; ?>'},
+                    {onload: function() {
+                            alert("Tracking request is loaded");
+                        }});
                 </script>
             </div>
             <div class="element-email">
